a4ee71f2ec4a9a487def6f98ae817f4921d61af0
Anel <anelpjanic@gmx.de>
2020-03-23 13:20:30 +0100

    test

---
+++ docs/listview.js
@@ -0,0 +1,48 @@
+export class ListView {
+  constructor(presenter) {
+    this.presenter = presenter;
+    this.app = document.getElementById('app');
+    console.log('ListView');
+    this.index=0;
+  }
+
+  render(personen) {
+    // Liste aller Personen
+    
+    let html = `
+      <h2>Liste aller Personen</h2>
+      <ul>`;
+    personen.forEach(person => {
+      const zeile = `
+      <li>${person.vorname} ${person.nachname}, Geb: ${person.geburtsdatum} <button id="buttonDelete${this.index}>X</button></li>
+      `;
+      html += zeile;
+      this.index++;
+    });
+
+    // Button "Neu"
+    html +=
+      `
+      </ul> <p></p>
+    <button id="buttonNew">Neue Person</button>`;
+
+    this.app.innerHTML = html;
+
+    // Eventhandler
+    const buttonNew = document.getElementById('buttonNew');
+    buttonNew.addEventListener('click', ()=>{
+      this.presenter.buttonNewClicked();
+    });
+
+    for(let i = 0; i<personen.length; i++){
+        const buttonId = `buttonDelete${i}`;
+        const button = document.getElementById(buttonId);
+        
+          button.addEventListener('click', ()=>{
+            this.presenter.buttonDeleteClicked(i);
+          });
+       
+    }
+  
+  }
+}
